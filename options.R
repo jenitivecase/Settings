7e7e0ff81772a7c99d0ce2d3bc43acd3a9d3baeb
jenitivecase <jbrussow@gmail.com>
2018-11-06 09:47:26 -0500

    initial commit

---
+++ options.R
@@ -0,0 +1,28 @@
+options(scipen = 999)
+options(stringsAsFactors = FALSE)
+
+date <- format.Date(Sys.Date(), "%Y%m%d")
+
+needed_packages <- c("dplyr", "tidyr", "openxlsx", "ggplot2")
+load_packages <- function(x) {
+  if (!(x %in% rownames(installed.packages()))) {
+    install.packages(x)
+  }
+  suppressPackageStartupMessages(require(x, character.only = TRUE))
+}
+sapply(needed_packages, load_packages)
+
+theme_set(theme_bw())
+
+#------------------red--------blue-----yellow------teal------orange------lime
+ATI_colors <- c("#E53935", "#80D8FF", "#FFD54F", "#009688", "#FF9800", "#CDDC39",
+                "#AB000D", "#49A7CC", "#C8A415", "#00675B", "#C66900", "#99AA00")
+		
+#------------------blue-------red-------green------purple-----lt blue------orange		
+NHA_colors <- c("#4f81bd", "#c0504d", "#9bbb59", "#8064a2", "#4bacc6", "#f79646")
+
+logit2prob <- function(logit){
+  odds <- exp(logit)
+  prob <- odds / (1 + odds)
+  return(prob)
+}
