35c624f840d63bec6c25a247930e0a2b2337813b
jenitivecase <jbrussow@gmail.com>
2019-08-06 16:53:20 -0500

    add fct_case_when function

--- options.R
+++ options.R
@@ -30,3 +30,10 @@ logit2prob <- function(logit){
   prob <- odds / (1 + odds)
   return(prob)
 }
+
+fct_case_when <- function(...) {
+  args <- as.list(match.call())
+  levels <- sapply(args[-1], function(f) f[[3]])  # extract RHS of formula
+  levels <- levels[!is.na(levels)]
+  factor(dplyr::case_when(...), levels=levels)
+}
